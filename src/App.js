df260ee593653ae577aa848038f2305d9b4604d3
Abdullahhashmi06 <64926823+Abdullahhashmi06@users.noreply.github.com>
2020-06-02 14:31:15 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -8,16 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h2>Abdullah Hashmi</h2>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>2nd Assignemnt - Base React App</p>
       </header>
     </div>
   );
